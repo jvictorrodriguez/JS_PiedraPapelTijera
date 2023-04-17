1da8334614539883c42a95e7651fb77ff9ae0679
jvictorrodriguez <vrodriguezbarbeira@gmail.com>
2023-04-18 00:23:44 +0200

    Tiene buena pinta

--- piedraPapelTijera.js
+++ piedraPapelTijera.js
@@ -5,38 +5,97 @@ var posibilidades = ["piedra", "papel", "tijera"];
 
 
 //Asignamos variables a elementos
+//inputs
 var nombres = document.getElementsByName("nombre");
 var partidas = document.getElementsByName("partidas");
+//span
+var actual= document.getElementById("actual");
+var total= document.getElementById("total");
+//botones
 var botonJugar=document.getElementsByTagName("button")[0];
 var botonYa=document.getElementsByTagName("button")[1];
 var botonReset=document.getElementsByTagName("button")[2];
+//variables globales
 var eleccionJugador=0;
 var eleccionMaquina=0;
 var resultado; // 0-empate  1-jugador1  2-Máquina
-
-
+var parametrosConfigurados; // Nombre y partidas informados correctamente
 
 
 
 //Añadimos Listeners
+//inputs
 nombres[0].addEventListener("blur", function(){
     comprobarNombre(nombres[0]);
 })
 
 partidas[0].addEventListener("blur", function(){
-   compruebaNumPartidas(partidas[0]);
+   comprobarNumPartidas(partidas[0]);
 })
 
+//Botones
+botonJugar.addEventListener("click",function(){
+    console.log("Presionaste el botón Jugar");
+   //Comprobamos si los valores de los imputs son correctos y actualizamos
+   //el contador de partidas
+ /*   console.log("Comprobar nombre es : "+ comprobarNombre(nombres[0]));
+   console.log("ComprobarNumPartidas es : "+ numPartidas(partidas[0])); */
+    if (comprobarNombre(nombres[0]) && comprobarNumPartidas(partidas[0])) {
+        console.log("Ha entrado en el if");
+        //Si ambos son correctos
+        parametrosConfigurados=true;
+        //bloqueamos la edición de los campos
+        // Nombre y Partidas
+        nombres[0].disabled="true";
+        partidas[0].disabled="true";
+        
+        actual=1;
+        total=partidas[0].value;
+
+       /*  
+        actual.innerHTML=actual;
+        total.textContent=total; */
+
+        document.getElementById("actual").innerHTML=actual;
+        document.getElementById("total").innerHTML=total;
+        
+    }
+   
+})
 botonYa.addEventListener("click",function(){
-    console.log("Presionaste el botón Ya");
-    seleccionMaquina();
-    var ganador= comprobarGanador();
-    historial(ganador);
+    /* console.log("Presionaste el botón Ya"); */
+    if(parametrosConfigurados &&  actual<=total){
+
+        seleccionMaquina();
+        var ganador= comprobarGanador();
+        historial(ganador);
+        document.getElementById("actual").innerHTML=actual;
+        actual++;
+    }
+})
+botonReset.addEventListener("click",function(){
+    console.log("Presionaste el botón Reset");
+    
+    document.querySelector("form").reset();
+    
+    parametrosConfigurados=false;
+    /* nombres[0]="";
+    nombres[0].focus;
+    partidas[0]=0; */
+
+
+
+
 })
 
 
-//Preparamos partida
-nombres[0].focus();
+
+/* // Añado el control sobre el evento "submit" del formulario. De esta forma, la función "procesarFormulario" solo recive el evento "submit"
+document.getElementsByTagName('form')[0].addEventListener("submit", procesarFormulario, false); */
+
+
+
+
 
 //Imágenes Jugador1
 var imagenes= document.getElementsByTagName("img");
@@ -129,7 +188,7 @@ function historial(ganador){
 // que el primer carácter no sea un número
 function comprobarNombre(nombre){
    
-    if(nombre.value.length > 3 && isNaN(nombre.value)) {
+    if(nombre.value.length > 3 && isNaN(nombre.value[0])) {
         console.log("correcto");
         nombre.classList.remove("fondoRojo");
         return true;
@@ -140,7 +199,7 @@ function comprobarNombre(nombre){
 }
 
 //Función que devuelve si true si es un número válido
-function compruebaNumPartidas(numPartidas){
+function comprobarNumPartidas(numPartidas){
     //Dado que el HTML controla que se introduce un número
     //Como convertimos a integer si es decimal solo coge la parte entera
     //Debemos comprobar que sea positivo y que sea un entero
